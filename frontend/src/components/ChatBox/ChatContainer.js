38cc76b140eb4fddecd1ea95fdd082fb9a33357e
jonesna213 <navyjones9@gmail.com>
2023-11-19 00:00:33 -0600

    Got the look of the page down, now i need to add functionality

--- frontend/src/components/ChatBox/ChatContainer.js
+++ frontend/src/components/ChatBox/ChatContainer.js
@@ -1,8 +1,48 @@
+import styles from "../../css/styles.module.css";
+import { useState } from "react";
+
 const ChatContainer = () => {
+    const [userInput, setUserInput] = useState("");
+    const [conversationStarted, setConversationStarted] = useState(false);
+
+    const inputChangeHandler = value => {
+        setUserInput(value);
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+        const input = event.target.userInput.value;
+        setUserInput("");
+        console.log(input);
+    }
+
     return (
-        <section className="col bg-dark text-white">
-            <div className="m-3">
-                <h1 className="text-center">How can I help you today?</h1>
+        <section className="col bg-dark text-white d-flex flex-column justify-content-between">
+            {conversationStarted ? (
+                <h1>Conversation</h1>
+            ) : (
+                <div className={`row ${styles.startingMessage}`}>
+                    <h3 className={`text-center`}>How can I assist you today?</h3>
+                </div>
+            )}
+
+            <div className={`row`}>
+                <form onSubmit={submitHandler} autoComplete="off" className={`w-50 mx-auto`}>
+                    <div className="input-group mb-1">
+                        <input
+                            type="text"
+                            name="userInput"
+                            value={userInput}
+                            onChange={event => { inputChangeHandler(event.target.value) }}
+                            className={`text-white form-control bg-dark ${styles.placeholderText}`}
+                            placeholder="Message ChatGPT..."
+                        />
+                        <button className="btn btn-outline-light" type="submit">
+                            Send
+                        </button>
+                    </div>
+                    <p className="text-secondary small text-center">Some answers may be incorrect. Consider verifying important questions.</p>
+                </form>
             </div>
         </section>
     );
--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -5,14 +5,23 @@ const SideBar = () => {
     return (
         <section className="col-2 text-white h-100">
             <div className="m-3">
-                <button type="button" className={`d-flex justify-content-between align-items-center w-100 ${styles.newChat} btn text-white`}>
-                    <span>New Chat</span>
+                <button type="button" className={`d-flex justify-content-between align-items-center w-100 ${styles.newChat} btn text-white fw-bold`}>
+                    New Chat
                     <PencilSquare />
                 </button>
 
-                <div>
-                    <p>test</p>
-                </div>
+                <section className="mt-4">
+
+                    {
+                        //loop through this inputting data
+                    }
+                    <div className="ms-1">
+                        <p className="ms-2 text-light-emphasis small mb-0">Date</p>
+                        <a className={`ps-2 py-1 d-block text-decoration-none text-white ${styles.prevConversation}`} href="w">Conversation name</a>
+                    </div>
+                </section>
+
+
             </div>
 
         </section>
