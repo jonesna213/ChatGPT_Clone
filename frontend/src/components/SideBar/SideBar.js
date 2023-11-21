03e1ef59609e0e2099dd3474fc651960b654a021
jonesna213 <navyjones9@gmail.com>
2023-11-20 20:50:18 -0600

    Added a edit menu popup for the rename and delte buttons

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,9 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PencilSquare from "../../assets/PencilSquare";
 import styles from "../../css/styles.module.css";
 import ThreeDots from "../../assets/ThreeDots";
+import Pencil from "../../assets/Pencil";
+import TrashCan from "../../assets/TrashCan";
 
 const SideBar = ({ chats, setCurrentConversation, currentConversation }) => {
+    const [isEditMenuOpen, setIsEditMenuOpen] = useState(false);
+
     useEffect(() => {
         const handleStorageChange = () => {
             console.log("localStorage changed");
@@ -19,9 +23,28 @@ const SideBar = ({ chats, setCurrentConversation, currentConversation }) => {
         };
     }, [setCurrentConversation]);
 
-    const openEditConversation = () => {
+    /*
+        Dont need to worry aobut ids because the ... only pops up for the current conversation
+    */
+    const handleEditButtonClick = event => {
+        event.stopPropagation();
 
-    }
+        setIsEditMenuOpen(!isEditMenuOpen);
+    };
+
+    const handleEditMenuOptionClick = (option) => {
+        // Implement logic for each menu option (e.g., delete or rename)
+        if (option === 'delete') {
+            // Handle delete logic
+            console.log('Delete clicked for conversation:', currentConversation);
+        } else if (option === 'rename') {
+            // Handle rename logic
+            console.log('Rename clicked for conversation:', currentConversation);
+        }
+
+        // Close the edit menu after handling the option
+        setIsEditMenuOpen(false);
+    };
 
     return (
         <section className="col-2 text-white h-100">
@@ -32,7 +55,6 @@ const SideBar = ({ chats, setCurrentConversation, currentConversation }) => {
                 </button>
 
                 <section className="mt-4">
-
                     {chats.map(convo => {
                         console.log("convo: ", convo);
                         return (
@@ -42,9 +64,34 @@ const SideBar = ({ chats, setCurrentConversation, currentConversation }) => {
                                 {currentConversation && currentConversation.id === convo.id ? (
                                     <div className={`d-flex justify-content-between align-items-center ps-2 my-1 text-white ${styles.currentConversation}`}>
                                         {convo.name}
-                                        <button onClick={openEditConversation} className={`btn ${styles.threeDotsButton}`}>
-                                            <ThreeDots />
-                                        </button>
+                                        <div className="position-relative">
+                                            <button onClick={handleEditButtonClick} className={`btn ${styles.threeDotsButton}`}>
+                                                <ThreeDots />
+                                            </button>
+
+                                            {isEditMenuOpen && (
+                                                <div className="position-absolute top-100 start-0">
+                                                    <div className="rounded p-2 bg-dark border">
+                                                        <ul className={`list-group ${styles.editMenu}`}>
+                                                            <li
+                                                                className="list-group-item btn px-5 d-flex align-items-center"
+                                                                onClick={() => handleEditMenuOptionClick('rename')}
+                                                            >   
+                                                                <Pencil />
+                                                                <span className="ms-2">Rename</span>
+                                                            </li>
+                                                            <li
+                                                                className="list-group-item btn px-5 d-flex align-items-center text-danger"
+                                                                onClick={() => handleEditMenuOptionClick('delete')}
+                                                            >
+                                                                <TrashCan />
+                                                                <span className="ms-2">Delete</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            )}
+                                        </div>
                                     </div>
                                 ) : (
                                     <a className={`ps-2 py-1 my-1 d-block text-decoration-none text-white ${styles.prevConversation}`} href="w">{convo.name}</a>
@@ -52,7 +99,6 @@ const SideBar = ({ chats, setCurrentConversation, currentConversation }) => {
                             </div>
                         );
                     })}
-
                 </section>
             </div>
         </section>
